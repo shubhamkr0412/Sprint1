9b293a223cca9ecf0b132753a4760a1d2c24c3a9
shubhamkr0412 <91714750+shubhamkr0412@users.noreply.github.com>
2022-01-03 17:25:49 +0530

    Create button.jsx

---
+++ myapp/src/button.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  font-size: 22px;
+  border: none;
+  margin-left: 100px;
+  cursor: pointer;
+  background-color: #b8e4f0;
+  border-radius: 5px;
+  width: 200px;
+  height: 50px;
+`;
